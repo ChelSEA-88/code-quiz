14a3e3531ea835fcfcac8bef1084f5f304b074b0
Chelsea Haviland <chelsea.haviland.13@gmail.com>
2020-07-22 22:02:00 -0400

    working start function

--- script.js
+++ script.js
@@ -1,64 +1,46 @@
-var quizBlock = document.querySelector(".card");
+//When we click start a timer will execute and the questions will appear
+//When user answers the question if/else for right or wrong and execute next question
+//When the user answer the question incorrectly we subtract time
+//wWhen all questions answered or time reaches zero, the game is over
+
+
+var quizBlock = document.querySelector(".card-body");
 var choiceBtn = document.querySelector(".btn-grid")
-var startButton = document.getElementById("#start-button");
+var startButton = document.getElementById("start-button");
 var questionSpace = document.getElementById("question");
 var choice1 = document.getElementById("choice1");
 var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var choice4 = document.getElementById("choice4");
-var questionElement = document.getElementById("questions");
-var nextQuestion;
+
 var score = 0;
 var questionIndex = 0;
 
 
-startButton.addEventListener("click", startQuiz());
-
-function startQuiz() {
-    startButton.classList.add("hide");
-    quizBlock.classList.remove("hide");
-    nextQuestion = 0;
-}
-
-function setNextQuestion() {
-    showQuestion(questions[nexQuestion++]);
-}
-
-
-//score plus conditions
-
-//if (userClick === "answer3"){
-//score++;
-//alert("Correct!");
-//} else {
-    //alert("Wrong!");
-//}
-
-
 var questions = [
     {
-    q : "Who designed the first mechanical computer?",
+    question : "Who designed the first mechanical computer?",
     choice1: "Bill Gates",
     choice2: "Dr. Phil",
     choice3: "Charles Babbage",
     choice4: "Albert Einstein",
     answer: "3"
 }, {
-    q: "Web pages are written using _____?",
+    question: "Web pages are written using _____?",
     choice1: "HTML",
     choice2: "URL",
     choice3: "The browser",
     choice4: "Javascript Only",
     answer: "1"
 }, {
-    q: "3Who designed the first mechanical computer?",
+    question: "Who designed the first mechanical computer?",
     choice1: "Bill Gates",
     choice2: "Dr. Phil",
     choice3: "Charles Babbage",
     choice4: "Albert Einstein",
     answer: "3"
  }, {
-    q: "4Who designed the first mechanical computer?",
+    question: "Who designed the first mechanical computer?",
     choice1: "Bill Gates",
     choice2: "Dr. Phil",
     choice3: "Charles Babbage",
@@ -66,8 +48,34 @@ var questions = [
      answer: "3"
 }];
 
+var setQuestions = questions.length -1;
+var currentQuestion = 0;
+
+function showQuestions() {
+    let q = questions.length();
+
+    questionSpace.innerText = ("<p>" + q.questions + "</p>");
+    choice1.innerText = q.choice1;
+    choice2.innerText = questions.choice1;
+}
 
 
+
+function startQuiz() {
+
+    showQuestions(questions[currentQuestion++]);
+    //displayquestion make them outside then call them in here
+    // startButton.classList.add("hide");
+    // quizBlock.classList.remove("hide");
+    // nextQuestion = 0;
+    // setNextQuestion();
+    //timer start
+    console.log("next question")
+}
+
+startButton.addEventListener("click", function () {
+    startQuiz()
+});
 
 // \n = line break
 
